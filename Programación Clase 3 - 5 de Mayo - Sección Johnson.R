5de064ede177264992e4e1fdd4c604aac59bbcda
Nataliejulian <44324000+Nataliejulian@users.noreply.github.com>
2022-05-05 16:18:28 -0400

    Speech Clase 3

--- Programación Clase 3 - 5 de Mayo - Sección Johnson.R
+++ Programación Clase 3 - 5 de Mayo - Sección Johnson.R
@@ -433,9 +433,9 @@ ggplot(data=viviendasRM, aes(x=comuna))+
 
 ########## FIN! MUCHAS GRACIAS POR SU ATENCION :)
 
-#algún comentario o sugerencia para el equipo? Please déjalo aquí!! :D
+#algún comentario o sugerencia para el equipo? Please déjalo aquí: https://www.menti.com/msx2bfp3m3
 
-### https://www.menti.com/msx2bfp3m3
+
 
 
 #Referencias
