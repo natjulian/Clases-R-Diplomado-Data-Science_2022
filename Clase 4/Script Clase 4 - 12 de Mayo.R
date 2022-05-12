fd30271cf11e3b7b841c284b1d99f696f3dfe09a
Nataliejulian <44324000+Nataliejulian@users.noreply.github.com>
2022-05-12 18:12:26 -0400

    Clase 4

---
+++ Clase 4/Script Clase 4 - 12 de Mayo.R
@@ -0,0 +1,8 @@
+#Script en Vivo - Clase 4 - 12 de Mayo - Sección Johnson
+
+# Nuestro equipo:
+
+# - Natalie Julian (najulian@mat.uc.cl)
+# - Maite Vergara (Maite.vergara@uc.cl) 
+# - Esteban Rucan (errucan@uc.cl)
+
