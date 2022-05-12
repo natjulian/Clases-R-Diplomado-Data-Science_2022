013c0592f4b206444b792514dd2de2a4a925ea29
Nataliejulian <44324000+Nataliejulian@users.noreply.github.com>
2022-05-12 19:03:11 -0400

    Script

--- Clase 4/Script Clase 4 - 12 de Mayo.R
+++ Clase 4/Script Clase 4 - 12 de Mayo.R
@@ -6,3 +6,37 @@
 # - Maite Vergara (Maite.vergara@uc.cl) 
 # - Esteban Rucan (errucan@uc.cl)
 
+library(readxl)
+library(tidyverse)
+library(janitor)
+
+viviendasRM <- read_excel("Clase 4/Bases de datos/viviendasRM.xlsx")
+View(viviendasRM)
+
+head(viviendasRM)
+
+## que era lo primero que haciamos al cargar una base de datos........
+
+names(viviendasRM)
+
+viviendasRM<- viviendasRM %>% clean_names()
+
+names(viviendasRM) #ahora sí :)
+
+## revisar formato de las variables
+
+summary(viviendasRM)
+
+viviendasRM%>%
+  select(n_estacionamientos)
+
+unique(viviendasRM$n_estacionamientos)
+
+# cambiar los No por cero
+
+viviendasRM<-viviendasRM%>% 
+  mutate(n_estacionamientos=ifelse(tolower(n_estacionamientos)=='no',0, n_estacionamientos))
+
+
+viviendasRM%>%
+  select(n_estacionamientos)
